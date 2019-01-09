02249b981138dde410f87c4a8ac7d7a04380cd2f
Aslan <berezg86@gmail.com>
2019-01-09 15:43:34 +0300

    слайдер

--- js/common.js
+++ js/common.js
@@ -23,13 +23,23 @@ $(document).ready(function() {
     });
 
     // show toggle menu ===================================
-    // search-tour
-    $('.search-tour').click(function(){
+    $('.search-tour').click(function() {
         $('.toggleMenu').toggleClass('toggleMenu-show');
         $('.search-tour').toggleClass('search-tour_active');
-        $('.toggleMenu-close').click(function(){
+        $('.toggleMenu-close').click(function() {
             $('.toggleMenu').removeClass('toggleMenu-show');
         });
+
+        $(function() {
+            $(document).click(function(event) {
+                if ($(event.target).closest(".toggleMenu-show").length) return;
+                if ($(event.target).closest(".search-tour").length) return;
+                $('.toggleMenu').removeClass('toggleMenu-show');
+                $('.search-tour').removeClass('search-tour_active');
+                event.stopPropagation();
+            });
+        });
+
     });
 
     // Toggle menu tabs ===================================
@@ -45,8 +55,72 @@ $(document).ready(function() {
     });
     $('#tab-list a:first').click();
 
+    // slider =============================================
+    var mainSliderSelector = ".main-slider",
+        interleaveOffset = 0.5;
 
-
+    var mainSliderOptions = {
+        loop: true,
+        speed: 1000,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000
+        },
+        loopAdditionalSlides: 10,
+        grabCursor: true,
+        watchSlidesProgress: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        on: {
+            init: function() {
+                this.autoplay.stop();
+            },
+            imagesReady: function() {
+                this.el.classList.remove("loading");
+                this.autoplay.start();
+            },
+            slideChangeTransitionEnd: function() {
+                var swiper = this,
+                    captions = swiper.el.querySelectorAll(".caption");
+                for (var i = 0; i < captions.length; ++i) {
+                    captions[i].classList.remove("show");
+                }
+                swiper.slides[swiper.activeIndex]
+                    .querySelector(".caption")
+                    .classList.add("show");
+            },
+            progress: function() {
+                var swiper = this;
+                for (var i = 0; i < swiper.slides.length; i++) {
+                    var slideProgress = swiper.slides[i].progress,
+                        innerOffset = swiper.width * interleaveOffset,
+                        innerTranslate = slideProgress * innerOffset;
+                    swiper.slides[i].querySelector(".slide-bgimg").style.transform =
+                        "translate3d(" + innerTranslate + "px, 0, 0)";
+                }
+            },
+            touchStart: function() {
+                var swiper = this;
+                for (var i = 0; i < swiper.slides.length; i++) {
+                    swiper.slides[i].style.transition = "";
+                }
+            },
+            setTransition: function(speed) {
+                var swiper = this;
+                for (var i = 0; i < swiper.slides.length; i++) {
+                    swiper.slides[i].style.transition = speed + "ms";
+                    swiper.slides[i].querySelector(".slide-bgimg").style.transition =
+                        speed + "ms";
+                }
+            }
+        }
+    };
+    var mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
 
 
 }); //END READY
